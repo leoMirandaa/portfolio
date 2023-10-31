45ff3b6d6c72f57055dd04bf122c125fe7d4e733
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-10-31 13:47:16 -0700

    fix: delete duplicated icons index file

--- app/components/ui/icons/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./GithubSVG";
-export * from "./MailSVG";
-export * from "./LinkedInSVG";
-export * from "./Sk8ter";
-export * from "./MagentaKitchenSVG";
-export * from "./ShopApiSVG";
-export * from "./LinkSVG";
