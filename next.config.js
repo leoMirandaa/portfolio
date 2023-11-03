e9f513fae67c4c7101ae41f914ade503517ebe26
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-11-02 22:48:29 -0700

    fix: change img tag to Image next component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com", "images.unsplash.com"],
   },
 };
 
