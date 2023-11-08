fb16e03706ea6df902ea16061bb1d17fac748879
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-11-08 00:04:13 -0800

    fix: re-install nextUI by individual installation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/theme");
 
 const config: Config = {
   content: [
@@ -9,13 +9,7 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      // backgroundImage: {
-      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //   "gradient-conic":
-      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
-    },
+    extend: {},
   },
   darkMode: "class",
   plugins: [nextui()],
