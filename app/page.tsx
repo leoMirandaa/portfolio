079a85ff32beaa594d0e14c3c82d6180a9530f57
leo <leopoldo.miranda18@tectijuana.edu.mx>
2024-01-11 16:49:05 -0800

    fix: change home folder name to about

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { HomePage } from "./components/home/page";
+import { About } from "./components/about/page";
 
 export default function Home() {
   return (
     <main className="p-4 pb-12 max-w-6xl m-auto overflow-hidden 2xl:overflow-visible">
-      <HomePage />
+      <About />
     </main>
   );
 }
