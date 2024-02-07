21bd47ed20f4ddab7b8457b61ead0d17acf54f23
leo <leopoldo.miranda18@tectijuana.edu.mx>
2024-02-06 18:43:34 -0800

    fix: typo

--- app/components/tabSection/Projects.tsx
+++ app/components/tabSection/Projects.tsx
@@ -1,11 +1,11 @@
 import { ProjectList } from "@/app/utils";
-import { projectType } from "@/app/types";
+import { ProjectType } from "@/app/types";
 import { Project } from "../Project";
 
 export const Projects = () => {
   return (
     <main className="flex flex-col gap-20">
-      {ProjectList.map((project: projectType) => (
+      {ProjectList.map((project: ProjectType) => (
         <Project
           key={`project-item-${project.title}`}
           {...project}
